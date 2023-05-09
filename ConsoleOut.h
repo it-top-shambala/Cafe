918abed4f52c1b214158a251cf737344691e9dd1
Alexey Amursky <Amursky.amursky@yandex.ru>
2023-05-09 11:36:25 +0400

    Pre - finish

--- ConsoleOut.h
+++ ConsoleOut.h
@@ -10,11 +10,28 @@
 using namespace std;
 
 void PrintMenu() {
-	cout << "Menu: " << endl;
-	for (Pizza pizza : pizzas) {
-		cout << pizza.code << ". " << pizza.name << " - " << pizza.price << endl;
-	}
-	for (Drink drink : drinks) {
-		cout << drink.code << ". " << drink.name << " - " << drink.price << endl;
-	}
+    cout << "ћеню: " << endl;
+    for (Pizza pizza : pizzas) {
+        cout << pizza.code << ". " << pizza.name << " - " << pizza.price << endl;
+    }
+    for (Drink drink : drinks) {
+        cout << drink.code << ". " << drink.name << " - " << drink.price << endl;
+    }
 }
+
+void GetProductAndQuantity(char& choice, int& quantity) {
+    cout << "¬ведите код продукта и колличество: " << endl;
+    cin >> choice >> quantity;
+}
+
+void PrintCheck(Order& order) {
+    cout << "„ек:" << endl;
+    for (int i = 0; i < order.pizzaCount; i++) {
+        cout << pizzas[order.pizzas[i].code - 1].name << " - " << order.pizzas[i].price << endl;
+    }
+    for (int i = 0; i < order.drinkCount; i++) {
+        cout << drinks[order.drinks[i].code - 5].name << " - " << order.drinks[i].price << endl;
+    }
+    cout << "»того: " << order.total << endl;
+}
+
--- main.cpp
+++ main.cpp
@@ -17,60 +17,31 @@ int main() {
 	order.pizzaCount = 0;
 	order.drinkCount = 0;
 
-	PrintMenu();
-
 	char choice;
 	int quantity;
 
-	do {
-		cout << "Введите код продукта и колличество: " << endl;
-		cin >> choice >> quantity;
-		if (choice >= pizzas[0].code && choice <= pizzas[1].code) {
-			order.pizzas[order.pizzaCount].code = choice;
-			order.pizzas[order.pizzaCount].price = pizzas[choice - 1].price;
-			order.pizzaCount++;
-			order.total += quantity * pizzas[choice - 1].price;
-		}
-		else if (choice >= drinks[0].code && choice <= drinks[1].code) {
-			order.drinks[order.drinkCount].code = choice;
-			order.drinks[order.drinkCount].price = drinks[choice - 101].price;
-			order.drinkCount++;
-			order.total += quantity * drinks[choice - 101].price;
-		}
+	cout << "Добро пожаловать в Наш pizza shop!" << endl;
 
-		cout << "Хотите продолжить заказ? (y/n) ";
+	string userName;
+	cout << "Пожалуйста введите Ваше имя: ";
+	cin >> userName;
+	cout << "Здравствуйте, " << userName << "!" << endl;
+
+	PrintMenu();
+
+	do {
+		GetProductAndQuantity(choice, quantity);
+		AddToOrder(order, choice, quantity);
+
+		cout << "Хотите продожить заказ? (y/n) ";
 		cin >> choice;
 	} while (choice == 'y');
 
-	if (order.pizzaCount >= 5) {
-		order.pizzas[order.pizzaCount].price = 0;
-		order.pizzaCount++;
-		order.total -= pizzas[0].price;
-	}
+	ProcessOrder(order, quantity);
 
-	if (order.total > 50) {
-		order.total *= 0.8;
-	}
+	PrintCheck(order);
 
-	for (int i = 0; i < order.drinkCount; i++) {
-		if (order.drinks[i].price > 2 && quantity > 3) {
-			order.drinks[i].price *= 0.85;
-			order.total -= (order.drinks[i].price - drinks[order.drinks[i].code - 101].price) * quantity;
-		}
-	}
-
-	cout << "Заказ: " << endl;
-	for (int i = 0; i < order.pizzaCount; i++) {
-		cout << order.pizzas[i].name << " - " << order.pizzas[i].price << endl;
-	}
-
-	for (int i = 0; i < order.drinkCount; i++) {
-		cout << order.drinks[i].name << " - " << order.drinks[i].price << endl;
-	}
-
-	cout << "Итого: " << order.total << endl;
-
-	cout << "Спасибо за ваш заказ!";
+	cout << "Спасибо за Ваш заказ, " << userName << "!" << endl;
 
 	return 0;
 }
